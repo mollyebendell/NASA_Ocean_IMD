0470c1b3998e2297ae93962911775ff4c4397540
Myungin Lee <benedic.lee@gmail.com>
2023-11-12 20:21:49 -0500

    testing probuild for higher resolution

---
+++ NASA_Ocean/Assets/Scripts/Control.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float sensitivity = 5.0f;
+    void Update()
+    {
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        // Rotate the camera based on the mouse movement
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.eulerAngles += new Vector3(-mouseY * sensitivity, mouseX * sensitivity, 0);
+
+    }
+}
+
